eebf87b5b736a414137439aba08463f02b11521d
Beyza Oztaskin <oztaskinbeyza@gmail.com>
2026-05-13 02:14:35 +0300

    Fix: Restored AuthProvider wrapper in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' 
+import { AuthProvider } from './AuthContext.jsx' 
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* Tüm siteyi Auth (Güvenlik) kalkanının içine alıyoruz */}
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 )
